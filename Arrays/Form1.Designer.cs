b4712770ac894a07e81992f5d6c04113a7e37780
Denis Batista <denis.si2011@gmail.com>
2021-02-09 14:35:26 -0300

    Add project files.

---
+++ Arrays/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Arrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTesteVetor = new System.Windows.Forms.Button();
+            this.btnVetorTexto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTesteVetor
+            // 
+            this.btnTesteVetor.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnTesteVetor.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTesteVetor.ForeColor = System.Drawing.Color.Transparent;
+            this.btnTesteVetor.Location = new System.Drawing.Point(72, 33);
+            this.btnTesteVetor.Name = "btnTesteVetor";
+            this.btnTesteVetor.Size = new System.Drawing.Size(148, 54);
+            this.btnTesteVetor.TabIndex = 0;
+            this.btnTesteVetor.Text = "Teste Vetor";
+            this.btnTesteVetor.UseVisualStyleBackColor = false;
+            this.btnTesteVetor.Click += new System.EventHandler(this.btnTesteVetor_Click);
+            // 
+            // btnVetorTexto
+            // 
+            this.btnVetorTexto.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnVetorTexto.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVetorTexto.ForeColor = System.Drawing.Color.Transparent;
+            this.btnVetorTexto.Location = new System.Drawing.Point(72, 156);
+            this.btnVetorTexto.Name = "btnVetorTexto";
+            this.btnVetorTexto.Size = new System.Drawing.Size(148, 54);
+            this.btnVetorTexto.TabIndex = 1;
+            this.btnVetorTexto.Text = "Vetor Textos";
+            this.btnVetorTexto.UseVisualStyleBackColor = false;
+            this.btnVetorTexto.Click += new System.EventHandler(this.btnVetorTexto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnVetorTexto);
+            this.Controls.Add(this.btnTesteVetor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTesteVetor;
+        private System.Windows.Forms.Button btnVetorTexto;
+    }
+}
+
---
+++ Arrays/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTesteVetor_Click(object sender, EventArgs e)
+        {
+            int[] vetor = new int[4];
+            vetor[0] = 7;
+            vetor[1] = 3;
+            vetor[2] = 5;
+            vetor[3] = 1;
+            int valor = vetor[1];
+            Array.Sort(vetor);
+
+            for (int index=0; index<4; index++)
+            {
+                MessageBox.Show(vetor [index].ToString(), "Array");
+            }
+
+            /*Declarar e instanciar o vetor
+             * Atribuição de valores de acordo com cada índice
+             * Declara variável "valor" para acessar o vetor
+             * Array.Sort - Está classe ordena o vetor
+             * Comando for - Percorre o vetor em cada índice.
+             * Codição - Para index menor que 4 ( de 0 a 3 ), ela exibe uma msg para o vetor na MessagemBox.
+             * */
+        }
+
+        private void btnVetorTexto_Click(object sender, EventArgs e)
+        {
+            string[] semana = new string[7];
+            semana[0] = "Domingo";
+            semana[1] = "Segunda-Feira";
+            semana[2] = "Terça-Feira";
+            semana[3] = "Quarta-Feira";
+            semana[4] = "Quinta-Feira";
+            semana[5] = "Sexta-Feira";
+            semana[6] = "Sabado";
+
+            string valor = semana[1];
+
+            for (int index = 0; index < 7; index++)
+            {
+                MessageBox.Show(semana[index].ToString(), "Array");
+
+            }
+            /*Declarar e instanciar o vetor
+            * Atribuição de valores de acordo com cada índice
+            * Aqui no caso são os dias da semana ( string )
+             * Declara variável "valor" para acessar o vetor
+            * Por ser se tratar de string não será necessário ordenar.             * 
+            * Comando for - Percorre o vetor em cada índice.
+            * Codição - Para index menor que 7 ( de 0 a 6 ), ela exibe uma msg para o vetor na MessagemBox, ou seja cada dia da semana.
+            * */
+        }
+    }
+}
